99267006873dad6446e5f3aee80654fedf551b21
Patrick King <89878544+patrickgking03@users.noreply.github.com>
2023-06-27 14:30:44 -0700

    Reverting Changes to script.js

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ $(document).ready(function(){
   
       $('#menu').removeClass('fa-times');
       $('header').removeClass('toggle');
-  
+        
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+        
     });
   
     $('a[href*="#"]').on('click',function(e){
